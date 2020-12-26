33dec328bec5c88970ec97ab36b394ce89d8fe38
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-26 13:38:26 -0500

    almostFullCrud

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,7 +1,7 @@
 class DevicesController < ApplicationController
     def index
-        @devices = Device.all
-        render json: @devices.to_json(:include => {
+        devices = Device.all
+        render json: devices.to_json(:include => {
             :reports => {:only => [:sender, :message]}
         }, except: [:created_at, :updated_at, :device_id])
     end 
@@ -15,9 +15,19 @@ class DevicesController < ApplicationController
         render json: device.to_json
     end 
 
+    def edit 
+        device = Device.find(params[:id])
+    end 
+
+    def updated
+        device = Device.find(params[:id])
+        device.update(device_params)
+        render json: device.to_json
+    end 
+
     def show
-        @device = Device.find(params[:id])
-        render json: @device.to_json(:include => {
+        device = Device.find(params[:id])
+        render json: device.to_json(:include => {
             :reports => {:only => [:sender, :message]}
         }, except: [:created_at, :updated_at, :device_id])
     end 
