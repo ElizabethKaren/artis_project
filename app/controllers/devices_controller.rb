6192957741b0b6d5239e66462adb03d21578f021
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-26 13:15:55 -0500

    jsonAdded

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,9 +1,13 @@
 class DevicesController < ApplicationController
     def index
-        @devices = Device.all 
+        @devices = Device.all
+        render json: @devices.to_json(:include => {
+            :reports => {:only => [:sender, :message]}
+        }, except: [:created_at, :updated_at, :device_id])
     end 
 
     def show
         @device = Device.find(params[:id])
+        render json: @device.to_json 
     end 
 end
