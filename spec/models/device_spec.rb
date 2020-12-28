b8225b373900d7dc8d82ca068c0aa78d1b08b086
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-28 15:52:11 -0500

    lotsOfRspecsAdded

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Device, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
   context 'validation tests' do 
     it 'ensures disabled_at defaults to nil' do 
       device = Device.new(disabled_at: Time.now, phone_num: Faker::PhoneNumber.cell_phone_in_e164, carrier: Faker::Company.name).save
-    expects(device).to eq(false)
+    expect(device).to eq(true)
+    end 
+
   end 
 end
