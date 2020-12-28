7767f5d357d545dadd4739ce90d8b25b8968b4dc
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-28 16:47:49 -0500

    scopeTestAdded

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,6 +2,8 @@ class Device < ApplicationRecord
     has_many :heartbeats
     has_many :reports
     validates :phone_num, length: { in: 9..15 }
-    validates :disabled_at, acceptance: { accept: nil }
+    # validates :disabled_at, acceptance: { accept: nil }
+    scope :active_devices, -> { where(disabled_at: nil) }
+    scope :inactive_devices, -> { where(disabled_at: DateTime ) }
 
 end
--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -8,9 +8,32 @@ RSpec.describe Device, type: :model do
       expect(device).to eq(true)
     end 
 
-    it 'ensures disabled at defaults to nil' do 
-    device = Device.new(disabled_at: Time.now, phone_num: Faker::PhoneNumber.cell_phone_in_e164, carrier: Faker::Company.name).save
-    expect(device).to eq(false)
+    # it 'ensures disabled at defaults to nil' do 
+    # device = Device.new(disabled_at: Time.now, phone_num: Faker::PhoneNumber.cell_phone_in_e164, carrier: Faker::Company.name).save
+    # expect(device).to eq(false)
+    # end 
+
+  end
+  
+
+  context 'scope tests' do
+    let (:params) { {phone_num: Faker::PhoneNumber.cell_phone_in_e164, carrier: Faker::Company.name} }
+    before(:each) do
+      3.times do
+        Device.create(params)
+      end 
+
+      2.times do
+        device = Device.create(params.merge(disabled_at: DateTime.now))
+      end
+    end 
+
+    it 'should return active devices' do 
+      expect(Device.active_devices.size).to eq(3)
+    end 
+
+    it 'should return inactive devices' do 
+      expect(Device.inactive_devices.size).to eq(2)
     end 
 
   end 
