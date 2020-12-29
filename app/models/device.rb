3186e4ba4d5cd2b8f94905efd9e82c7a699f4ff9
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-29 12:00:40 -0500

    updatedMaybeDone

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,10 +1,19 @@
 class Device < ApplicationRecord
     has_many :heartbeats
     has_many :reports
+    before_validation :check_country_code, on: [ :create, :update ]
     validates :phone_num, presence: true 
     validates :carrier, presence: true 
-    validates :phone_num, length: { in: 9..15 }
+    validates :phone_num, length: { in: 10..14 }
     scope :active_devices, -> { where(disabled_at: nil ) }
     scope :inactive_devices, -> { where.not(disabled_at: nil ) }
+
+    private 
+
+     def check_country_code
+        if self.phone_num && self.phone_num.size == 10
+            self.phone_num = '1' + self.phone_num
+        end 
+     end 
 
 end
