cd9210f1db901fd106e9020fa6dacabac0a6db37
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2020-12-30 19:24:59 -0500

    heartbeatValidationWorks

--- app/models/heartbeat.rb
+++ app/models/heartbeat.rb
@@ -1,15 +1,12 @@
 class Heartbeat < ApplicationRecord
-    belongs_to :device 
-
-    validates :device_id, presence: { message: "invalid phone number" }
+    belongs_to :device
 
     validates :is_device_disabled, presence: { message: "invalid phone number" }
 
     private 
 
     def is_device_disabled
-        # self.device.disabled_at == nil
-        Devices.active_devices.include?(self.device)
+        self.device.disabled_at == nil
     end
 
 end
--- spec/models/heartbeat_spec.rb
+++ spec/models/heartbeat_spec.rb
@@ -5,14 +5,9 @@ RSpec.describe Heartbeat, type: :model do
 
   context 'validation tests' do 
 
-    it 'ensures presence of device id' do 
-      heartbeat = Heartbeat.new(device_id: nil ).save
-      expect(heartbeat.device).to eq(false) 
-    end 
-
-    it 'ensures device is active' do 
-      heartbeat = Heartbeat.new(device_id: Device.inactive_devices.first.id ).save
-      expect(heartbeat.device).to eq(false) 
+    it 'ensures device is active' do
+      heartbeat = Heartbeat.new( device_id: Device.inactive_devices.sample.id ).save
+      expect(heartbeat).to eq(false) 
     end 
 
   end 
